786cafb781c069e5697c6e019f70e2089415f8e4
Sachin Raut <imsachinraut@gmail.com>
2020-03-22 13:10:17 +0530

    Function example

--- 01 Go programming basics - Todd McLeod/10function.go
+++ 01 Go programming basics - Todd McLeod/10function.go
@@ -5,9 +5,31 @@ import "fmt"
 func main(){
 	s := foo("Mumbai")
 	fmt.Println(s)
+
+	fn, ln := name()
+	fmt.Println(fn)
+	fmt.Println(ln)
+
+	//variadic means 0 or more parameters
+	bar(2,3,4)
+
+	b := []int{4,6,8}
+	bar(b...)
 }
 
 func foo(s string) string {
 	a := "Hello " + s
 	return a
+}
+
+func name()(string, string){
+	return "sachin","raut"
+}
+
+func bar(x ...int){
+	avg := 0
+	for _,v := range x {
+		avg += v
+	}
+	fmt.Println(avg)
 }
