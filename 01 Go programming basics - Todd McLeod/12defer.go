f3f3e4d050ed18126af4debccd3d8614ce2046fd
Sachin Raut <imsachinraut@gmail.com>
2020-03-23 00:07:40 +0530

    Defer example

--- 01 Go programming basics - Todd McLeod/12defer.go
+++ 01 Go programming basics - Todd McLeod/12defer.go
@@ -2,14 +2,6 @@
 create a function that opens 2 files & copies content of 1 file into another
 */
 
-
-/*
-This program will work, but there is a bug.
-If the call to "os.Create()" fails, then the function will return without
-closing the source file. Let's fix this problem in next program.
-*/
-
-
 package main
 
 import (
@@ -36,3 +28,38 @@ func copyFile(fromFile, toFile string) {
 	dst.Close()
 	src.Close()
 }
+
+
+/*
+This program will work, but there is a bug.
+If the call to "os.Create()" fails, then the function will return without
+closing the source file. 
+
+Let's fix this bug, using "defer"
+*/
+
+func copyFile(fromFile, toFile string){
+	src, err := os.Open(fromFile)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.Create(toFile)
+	if err != nil {
+		return
+	}
+	defer dst.Close()
+
+	written, err := io.Copy(dst, src)
+}
+
+/*
+1. "defer" ensures/guarantees that the defer statement would be executed later
+2. defer is used to perform clean up actions
+3. In this example, defer statement guarantees that the files will be closed 
+before returning control out of the function.
+4. Let's assume, the call to "os.Create" fails, so it return. But before that
+"defer src.Close()" will be executed. 
+*/
+
