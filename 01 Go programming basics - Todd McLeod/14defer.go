74e9a031fe64047a8cab572fc19dc40951142f13
Sachin Raut <imsachinraut@gmail.com>
2020-03-23 15:39:25 +0530

    defer example

--- 01 Go programming basics - Todd McLeod/14defer.go
+++ 01 Go programming basics - Todd McLeod/14defer.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main(){
+
+	//deferred function calls are executed in last in first out order (LIFO)
 	defer fmt.Println("defer 1")
 	defer fmt.Println("defer 2")
 	fmt.Println("Hi")
