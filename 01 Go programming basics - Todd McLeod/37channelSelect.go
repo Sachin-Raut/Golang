8f4342fde30d298a12d6a1661be6fb9eb8202832
Sachin Raut <imsachinraut@gmail.com>
2020-03-27 18:44:07 +0530

    channel select example

---
+++ 01 Go programming basics - Todd McLeod/37channelSelect.go
@@ -0,0 +1,46 @@
+// we will see how to range over a channel
+
+package main
+
+import "fmt"
+
+func main(){
+	evenChannel := make(chan int)
+	oddChannel := make(chan int)
+	quitChannel := make(chan int)
+
+	//send channel
+	go send(evenChannel, oddChannel, quitChannel)
+
+	//receive channel
+	receive(evenChannel, oddChannel, quitChannel)
+}
+
+func send(e, o, q chan <- int) {
+	for i := 0; i < 100; i++ {
+		
+		//put even number on evenChannel
+		if i%2 == 0 {
+			e <- i
+		} else {
+		
+		//put odd number on oddChannel
+		o <- i
+		}		
+	}
+	q <- 0
+}
+
+func receive(e, o, q <- chan int){
+	for {
+		select {
+		case v := <- e:
+			fmt.Println("even channel -", v)
+		case v := <- o:
+			fmt.Println("odd channel -", v)
+		case v := <- q:
+			fmt.Println("quit channel -", v)
+			return
+		}
+	}
+}
