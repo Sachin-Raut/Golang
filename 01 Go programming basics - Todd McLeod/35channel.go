6be1471d45e1eda1bb36c729777a114071c74154
Sachin Raut <imsachinraut@gmail.com>
2020-03-27 14:14:40 +0530

    channel example

---
+++ 01 Go programming basics - Todd McLeod/35channel.go
@@ -0,0 +1,25 @@
+/*
+
+1. channels are reference types
+2. channels are better way for synchronising code & writing concurrent code
+3. channels are pipes that connect concurrent goroutines
+4. you can send values from 1 goroutine, & receive that value in another goroutine
+5. by default, channels are "unbuffered", meaning that they will only accept send (chan <- )
+   if there is a corresponding receive (<- chan)
+
+*/
+
+package main 
+
+import "fmt"
+
+func main() {
+	
+	ch := make(chan int)
+
+	go func(){
+		ch <- 20
+	}()
+
+	fmt.Println(<- ch)
+}
