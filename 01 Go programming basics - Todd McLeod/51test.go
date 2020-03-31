db1eb888fddcaa151bb76a624096ff342d139211
Sachin Raut <imsachinraut@gmail.com>
2020-03-31 11:56:10 +0530

    test info

---
+++ 01 Go programming basics - Todd McLeod/51test.go
@@ -0,0 +1,20 @@
+/*
+
+1. test ensures that your code is doing what you want it to do
+2. tests must be in a file that ends with "_test.go"
+3. test file should be in the same package as the one being tested.
+4. test must be in a function with an signature "func Testxxx(t *testing.T)" 
+
+if you want to test following function 
+
+func mySum(){
+
+}
+
+then test function should be 
+
+func TestMySum(t *testing.T){
+
+}
+
+*/
