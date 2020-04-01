edc03fee76325fc0a8d929e3d0cd2acf04672473
Sachin Raut <imsachinraut@gmail.com>
2020-04-01 20:26:37 +0530

    multiple return

--- 02 Go Developers Guide by Stephen Grider/01multipleReturn.go
+++ 02 Go Developers Guide by Stephen Grider/01multipleReturn.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
-import "strings"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+	"os"
+)
+
+type deck []string
 
 //example of multiple return values
 
@@ -18,6 +24,25 @@ func main() {
 	 slice1 := []string{"Mumbai","Pune"}
 	 slice2 := strings.Join(slice1,",")
 	 fmt.Println(slice2)
+
+	 //save data to file "city_name" on hard drive
+	 ioutil.WriteFile("city_name", []byte("abcd"), 0666)
+
+	 //read data from file "city_name" on hard drive
+	 byteSlice, err := ioutil.ReadFile("city_name")
+	if err != nil {
+		fmt.Println("Error -", err)
+		os.Exit(1)
+
+		/*
+		Exit(0) - it indicates success
+		Exit(1) - it indicates an error
+		
+		Exit terminates the program immediately, "deferred functions" are not executed
+		*/
+	}
+	normalString := string(byteSlice)
+	fmt.Println(normalString)
 
 }
 
