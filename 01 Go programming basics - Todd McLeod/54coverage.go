69aede145f1dd0f83e29d3727abc76399bc2a7f3
Sachin Raut <imsachinraut@gmail.com>
2020-04-01 12:12:48 +0530

    coverage

--- 01 Go programming basics - Todd McLeod/54coverage.go
+++ 01 Go programming basics - Todd McLeod/54coverage.go
@@ -1,6 +1,6 @@
 /*
 
 1. Coverage means, how much code you have written is covered by tests.
-2. Cover as much code as possible. In prctise, it's almost possible to cover 100% code.
+2. Cover as much code as possible. In practise, it's almost impossible to cover 100% code.
 
 */
