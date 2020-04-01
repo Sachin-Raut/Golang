a424f83b6e94209c7c02374725523e208ac57b3b
Sachin Raut <imsachinraut@gmail.com>
2020-04-01 18:21:17 +0530

    multiple return

--- 02 Go Developers Guide by Stephen Grider/01multipleReturn.go
+++ 02 Go Developers Guide by Stephen Grider/01multipleReturn.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "strings"
 
 //example of multiple return values
 
@@ -8,6 +9,15 @@ func main() {
 
 	a, b := multipleReturn()
 	fmt.Println("a =", a, "\nb =", b)
+
+
+	cityName := "Mumbai"
+	fmt.Println([]byte(cityName)) //array of ascii values byteArray
+
+	//Joining the slice of strings
+	 slice1 := []string{"Mumbai","Pune"}
+	 slice2 := strings.Join(slice1,",")
+	 fmt.Println(slice2)
 
 }
 
