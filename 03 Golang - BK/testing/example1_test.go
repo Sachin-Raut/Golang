dcd23cc261af1abd00ca0df6448fdbb20e7096f5
Sachin Raut <imsachinraut@gmail.com>
2020-04-04 12:00:46 +0530

    testing

---
+++ 03 Golang - BK/testing/example1_test.go
@@ -0,0 +1,44 @@
+package testing
+
+import (
+	"net/http"
+	"testing"
+)
+
+const succeed = "\u2713"
+const failed = "\u2717"
+
+func TestDownload(t *testing.T){
+	url := "https://www.goinggo.net/post/index.xml"
+
+	statusCode := 200
+
+	t.Log("Given the need to test downloading content")
+	{
+		t.Logf("\t test 0 = \t When checking %q for status code %d", url, statusCode)
+		{
+			resp, err := http.Get(url)
+			if err != nil {
+				t.Fatalf("\t%s\tShould be able to make the Get call - %v", failed, err)
+			}
+
+			t.Logf("\t %s \t Should be able to make the Get call", succeed)
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode == statusCode {
+				t.Logf("\t %s \t Should receive a %d status code", succeed, statusCode)
+			} else {
+				t.Errorf("\t %s \t Should receive a %d status code - %d", failed, statusCode, resp.StatusCode)
+			}
+		}
+	}
+}
+
+/*
+
+1. $ go test
+2. $ go test -v
+3. $ go test -run Down
+
+*/
