a921b2f7d31b468ad251cfb329cbabce4df4f3a4
Sachin Raut <imsachinraut@gmail.com>
2020-04-04 13:50:06 +0530

    wait group

---
+++ 02 Go Developers Guide by Stephen Grider/06waitGroup.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main(){
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go printEven(i, &wg)
+	}
+
+	wg.Wait()
+}
+
+func printEven(x int, wg *sync.WaitGroup){
+	if x % 2 == 0 {
+		fmt.Printf("%d is even\n", x)
+	}
+	wg.Done()
+}
+
+/*
+
+1. in this example, our goroutines are communicating by sharing memory which is wrong.
+2. goroutines should communicate via channels
+3. channels are like pipes with 2 ends. Messages go in one end when sent, & comes out 
+at the other end when requested by the receiver.
+
+*/
