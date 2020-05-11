5bb725ddfb609c679ee2490dab8b90b924527535
Sachin Raut <imsachinraut@gmail.com>
2020-05-11 12:07:08 +0530

    httptest package

--- 04 Hands on Microservices with Go/04httpTest/main.go
+++ 04 Hands on Microservices with Go/04httpTest/main.go
@@ -9,6 +9,16 @@ import (
 func main(){
 	fmt.Println("hi")
 	http.HandleFunc("/example",handlers.MyHandler)
-	http.ListenAndServe(":8080", nil)
-	fmt.Println("hi1")
+
+	// http.ListenAndServe(":8080", nil)
+	// fmt.Println("hi1")
+
+	srv := &http.Server {
+		Addr : ":8080",
+		ReadTimeout : 10 * time.Second,
+		WriteTimeout : 10 * time.Second,
+		MaxHeaderBytes : 1 << 20,		
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
