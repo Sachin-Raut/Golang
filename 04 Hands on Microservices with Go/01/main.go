a3fe0f0b764bc84a90e27ca659a3a20e4603695d
Sachin Raut <imsachinraut@gmail.com>
2020-05-11 11:55:15 +0530

    1st microservice

--- 04 Hands on Microservices with Go/01/main.go
+++ 04 Hands on Microservices with Go/01/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main(){
 	http.HandleFunc("/",handler)
 
 	//start the server
-	http.ListenAndServe("localhost:8080", nil)
+	// http.ListenAndServe("localhost:8080", nil)
+
+	srv := &http.Server {
+		Addr : ":8000",
+		ReadTimeout : 10 * time.Second,
+		WriteTimeout : 10 * time.Second,
+		MaxHeaderBytes : 1 << 20,		
+	}
+
+	srv.ListenAndServe()
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
