8228ca6bb84f1c2257e5260525685b4d51870205
Sachin Raut <imsachinraut@gmail.com>
2020-05-11 11:57:54 +0530

    timezone

--- 04 Hands on Microservices with Go/02/main.go
+++ 04 Hands on Microservices with Go/02/main.go
@@ -28,7 +28,16 @@ func main(){
 	http.HandleFunc("/",handler)
 
 	//start the server
-	http.ListenAndServe("localhost:8080", nil)
+	// http.ListenAndServe("localhost:8000", nil)
+
+	srv := &http.Server {
+		Addr : ":8000",
+		ReadTimeout : 10 * time.Second,
+		WriteTimeout : 10 * time.Second,
+		MaxHeaderBytes : 1 << 20,		
+	}
+
+	srv.ListenAndServe()
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
